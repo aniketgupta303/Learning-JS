f387fa865413be9f6b998bc3383da6700cad98fb
Aniket  Gupta <117922196+aniketgupta303@users.noreply.github.com>
2024-03-06 08:03:15 +0000

    Function

---
+++ 01_basics/Practice.js
@@ -0,0 +1,50 @@
+/*console.log("aniket") 
+//print 0-10
+for(i=0; i<11; i++){
+    console.log(i);
+}
+
+//print 24-50
+for(i=24; i<51; i++){
+    console.log(i);
+}
+
+//while
+
+var a=23;
+
+ while(a<50){
+    a++;
+ }
+console.log(a); 
+
+//Functions- code ko naam dena 
+
+function abcd{
+    //funcatin Statement
+}
+
+//code reuse 
+function Gupta(){
+console.log("Aniket")
+console.log("Abhishek")
+console.log("Harsh")
+console.log("bunty")}
+
+Gupta();
+
+// want to run code every time  with differnt data
+function abcd(a,b,c){
+    console.log(a,b,c);
+}
+
+abcd(10,2,33);
+*/
+//10,2,33- arugument
+//a,b,c- parameter
+
+//argument-real value jo hum dete hai fun chalate time
+//parameter-variable jinme value store hoti hai argument waali 
+
+//Arrays
+
